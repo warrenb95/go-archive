4e4927e2d17c72b585d32c5104577a12fe9663d8
Warren <warrenb95@live.co.uk>
2024-11-03 21:34:11 +0000

    just using zip to zip folders not, gzip not needed (: need to RTFM lol

--- main.go
+++ main.go
@@ -2,47 +2,43 @@ package main
 
 import (
 	"archive/zip"
+	"flag"
+	"fmt"
 	"log"
 	"os"
-
-	"github.com/warrenb95/go-archive/internal/compresser"
 )
 
+var sourceDir string
+
 func main() {
+	flag.StringVar(&sourceDir, "s", "", "The directory you want to zip up and compress")
+	flag.Parse()
+
 	logFile, err := os.OpenFile("compressor.log", os.O_APPEND|os.O_CREATE|os.O_RDWR, os.ModePerm)
 	if err != nil {
 		log.Fatalf("failed to create compressor.log file: %v", err)
 	}
 	log.SetOutput(logFile)
 
-	zipFile, err := os.Create("archive.zip")
+	if sourceDir == "" {
+		log.Fatal("-s source and -o output need to be specified")
+	}
+
+	zipPath := fmt.Sprintf("%s.zip", sourceDir)
+	zipFile, err := os.Create(zipPath)
 	if err != nil {
 		log.Fatalf("creating zip file: %v", err)
 	}
+	log.Printf("created zip file %s", zipPath)
 
 	w := zip.NewWriter(zipFile)
-	err = w.AddFS(os.DirFS("./internal/compresser/test_data"))
+	err = w.AddFS(os.DirFS(sourceDir))
 	if err != nil {
 		log.Fatalf("Failed to add e/archive to zip writter: %v", err)
 	}
 	err = w.Close()
 	if err != nil {
 		log.Fatalf("closing zip writer: %v", err)
-	}
-
-	err = zipFile.Close()
-	if err != nil {
-		log.Fatalf("failed to close zip file: %v", err)
-	}
-
-	zipFile, err = os.Open("archive.zip")
-	if err != nil {
-		log.Fatalf("opening zip file: %v", err)
-	}
-
-	err = compresser.Compress("archive.zip.gzip", zipFile)
-	if err != nil {
-		log.Fatalf("compressing archive file: %v", err)
 	}
 
 	err = zipFile.Close()
